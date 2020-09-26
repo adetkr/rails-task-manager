f636f836f1d2a29824d3b623b8b9fcfcc4556753
ademola toukourou <tkradm@yahoo.fr>
2020-09-26 15:47:24 +0200

    task manager done, unstyled

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+  def destroy
+    @task.destroy
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to index_path
+  end
+
+  def new
+
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  private
+  def set_task
+    @task = Task.find(params["id"])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  get 'index', to: "tasks#index"
+  get 'tasks/new', to: "tasks#new"
+  post "tasks/new", to: "tasks#create"
+  get 'tasks/:id', to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id/edit", to: "tasks#update", as: :update
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
